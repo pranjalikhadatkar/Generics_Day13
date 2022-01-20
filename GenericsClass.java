e5d51aad9c0d679eb7f8b2c6756b26c215a6f78b
pranjali khadatkar <khadatkarpranjali1994@gmail.com>
2022-01-20 18:20:34 +0530

    Added: Refactoring all 3 datatype in one generic method

--- GenericsClass.java
+++ GenericsClass.java
@@ -1,18 +1,22 @@
 
-public class GenericsClass {
-	public static void findMaxString(String x, String y, String z) {
-		String max=x;
-		if(y.compareTo(max)>0) {
-			max=y;
-		}
-		if(z.compareTo(max)>0) {
-			max=z;
-		}
-		System.out.printf("Maximum of %s, %s, and %s is %s\n" , x, y, z, max);
-	}
-public static void main(String[] args) {
-	String x="apple", y="peach", z="banana";
-	findMaxString(x, y, z);
-}
-	
-}
+public class GenericsClass<T> {
+	 public static <T extends Comparable> T getMax(T first, T second, T third) {
+	        if (first.compareTo(second) > 0 && first.compareTo(third) > 0) {
+	            return first;
+	        } else if (second.compareTo(first) > 0 && second.compareTo(third) > 0) {
+	            return second;
+	        } else {
+	            return third;
+	        }
+	    }
+
+	    public static void main(String[] args) {
+	        Integer firstInt = 1, secondInt = 5, thirdInt = 9;
+	        Float firstFloat = 15.7f, secondFloat = 8.2f, thirdFloat = 6.2f;
+	        String firstString = "Nick", secondString = "john", thirdString = "Sachin";
+	        
+	        System.out.println(getMax(firstInt, secondInt, thirdInt));
+	        System.out.println(getMax(firstFloat, secondFloat, thirdFloat));
+	        System.out.println(getMax(firstString, secondString, thirdString));       
+	    }	
+ }
