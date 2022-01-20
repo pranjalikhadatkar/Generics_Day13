1b8d52159bc4de90d8da43cb6db6a7c47a9b64b5
pranjali khadatkar <khadatkarpranjali1994@gmail.com>
2022-01-20 17:07:40 +0530

    Added: finding maximum double value using compairableTo method

--- GenericsClass.java
+++ GenericsClass.java
@@ -1,7 +1,7 @@
 
 public class GenericsClass {
-	public static void findMaxInteger(Integer x, Integer y, Integer z) {
-		Integer max=x;
+	public static void findMaxDouble(Double x, Double y, Double z) {
+		Double max=x;
 		if(y.compareTo(max)>0) {
 			max=y;
 		}
@@ -11,7 +11,7 @@ public class GenericsClass {
 		System.out.printf("Maximum of %s, %s, and %s is %s\n" , x, y, z, max);
 	}
 public static void main(String[] args) {
-	int x=50, y=30, z=35;
-	findMaxInteger(x, y, z);
-}
+	Double x=3.2, y=12.5, z=9.4;
+	findMaxDouble(x, y, z);
+ }
 }
