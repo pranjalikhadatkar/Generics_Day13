48e82ae97e7fa5671d05efa1494c0daadf1035ea
pranjali khadatkar <khadatkarpranjali1994@gmail.com>
2022-01-23 23:42:00 +0530

    Added: Refactoring to generic class

--- GenericsClass.java
+++ GenericsClass.java
@@ -1,22 +1,52 @@
 
-public class GenericsClass<T> {
-	 public static <T extends Comparable> T getMax(T first, T second, T third) {
-	        if (first.compareTo(second) > 0 && first.compareTo(third) > 0) {
-	            return first;
-	        } else if (second.compareTo(first) > 0 && second.compareTo(third) > 0) {
-	            return second;
-	        } else {
-	            return third;
-	        }
-	    }
+public class GenericsClass<T extends Comparable<T>> {
 
+	    public static void getMaxInt(Integer first , Integer second , Integer third ) {
+	    	Integer max = first;
+	        if (second.compareTo(max) > 0 ) {
+	            max = second;
+	        }
+	        if (third.compareTo(max) > 0){
+	            max = third;
+	        }
+	        printMax(first , second , third , max);
+	    }
+	    
+	    public static void getMaxDouble(Double first , Double second , Double third ) {
+	    	Double max = first;
+	        if (second.compareTo(max) > 0 ) {
+	            max = second;
+	        }
+	        if (third.compareTo(max) > 0){
+	            max = third;
+	        }
+	        printMax(first , second , third , max);
+	    }
+	    public static String getMaxStr(String a , String b , String c ) {
+	        String max = a ;
+	        if (b.compareTo(max) > 0 ) {
+	            max = b;
+	        }
+	        if (c.compareTo(max) > 0) {
+	            max = c;
+	        }
+	        printMax(a , b , c , max);
+	        return max;
+	    }
+	    public static <T> void printMax(T a, T b, T c, T max) {
+	        System.out.printf("Max of %s, %s and %s is %s\n",a,b,c,max);
+	    }
 	    public static void main(String[] args) {
-	        Integer firstInt = 1, secondInt = 5, thirdInt = 9;
-	        Float firstFloat = 15.7f, secondFloat = 8.2f, thirdFloat = 6.2f;
-	        String firstString = "Nick", secondString = "john", thirdString = "Sachin";
-	        
-	        System.out.println(getMax(firstInt, secondInt, thirdInt));
-	        System.out.println(getMax(firstFloat, secondFloat, thirdFloat));
-	        System.out.println(getMax(firstString, secondString, thirdString));       
-	    }	
- }
+	        Integer firstInt = 15, secondInt = 32, thirdInt = 13;
+	        Double firstDouble = 1.2, secondDouble = 8.2, thirdDouble = 60.2;
+	        String firstString = "Virat", secondString = "Dhoni", thirdString = "Sachin";
+
+	        GenericsClass<Integer> Generic1 = new GenericsClass<Integer>();
+	        GenericsClass<Float>  Generic2 = new GenericsClass<Float>();        
+	        GenericsClass<String> Generic3= new GenericsClass<String>();
+	     
+	        Generic1.getMaxInt(firstInt, secondInt, thirdInt);
+	       Generic2.getMaxDouble(firstDouble, secondDouble, thirdDouble);
+	       Generic3.getMaxStr(firstString, secondString, thirdString);     
+	    }
+}
